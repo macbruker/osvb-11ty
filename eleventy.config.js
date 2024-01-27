1b25e07aca149c93e8e3a97fbcebb2f4165c9329
Martin Berglund <marberglund@gmail.com>
2024-01-27 20:56:16 +0100

    Testing TinaCMS

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy({"./node_modules/decap-cms-app/dist/decap-cms-app.js" : "assets/js/decap-cms.js"});
 
     return {
         dir: {
-            input: 'src',
-            output: 'dist'
+            input: 'collections',
+            includes: '../templates',
+            output: 'static'
         }
     }
 }
